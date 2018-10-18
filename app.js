fc4274c0381b860d60da3c4f561475e8741ff7ef
itachi73 <s.afifi170@gmail.com>
2018-10-18 06:56:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type', 'text/html');
+
+res.end('<center><h1>Hello World</h1>sh</center>');
+
+});
+
+server.listen(port,() => {
+
+console.log(`Server running at port `+port);
+
+});
