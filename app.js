f1b6babfbf6a1dc8f48c085dc51bbe24646f7b56
Oumeir <39211458+Oumeir@users.noreply.github.com>
2019-06-02 05:01:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ res.statusCode = 200;
 
 res.setHeader('Content-Type', 'text/html');
 
-res.end('<center><h1>hi from  oumier saif </center>');
+res.end('<center><h1>subdomain takeover</h1> </center>');
 
 });
 
