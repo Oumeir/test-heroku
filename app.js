ad80e0b5a1bbf9122acfa4d180fd4405bfc72946
Oumeir <39211458+Oumeir@users.noreply.github.com>
2019-06-09 22:28:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ res.statusCode = 200;
 
 res.setHeader('Content-Type', 'text/html');
   
-  
+<script>alert("XSS")</script>
 res.end('<center><h1>subdomain takeover By xJohnCode!</h1> </center>');
 
 });
