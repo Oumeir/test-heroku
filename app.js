5de87a5c20563c442b23578adb3e17566676a551
Oumeir <39211458+Oumeir@users.noreply.github.com>
2019-06-09 22:26:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 res.statusCode = 200;
 
 res.setHeader('Content-Type', 'text/html');
-response.end('<img src=x onerror=alert("XSS")>');
+<img src=x onerror=alert("XSS")>
 res.end('<center><h1>subdomain takeover By xJohnCode!</h1> </center>');
 
 });
